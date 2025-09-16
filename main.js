4a59ddee0c61c1f908d7e640af433812c44db8e8
itsxelliex-cyber <itsxelliex@gmail.com>
2025-09-16 23:25:07 +0000

    finished the first part of the jam! looking forward to the rest of the parts

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 const input = document.getElementById('input');
-const audioCtx = newAudioContent();
+const audioCtx = new AudioContext();
 const gainNode = audioCtx.createGain();
-const oscillator = audioCtx.createOscillator();
+const oscillator= audioCtx.createOscillator();
+oscillator.start();
+gainNode.gain.value = 0;
+audioCtx.resume();
+gainNode.gain.value = 0;
 oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 oscillator.type = "sine";
+notenames = new Map();
+notenames.set("C",261.6);
+notenames.set("D",293.7);
+notenames.set("E",329.6);
+notenames.set("F",349.2);
+notenames.set("G",392.0);
+notenames.set("A",440.0);
+notenames.set("B",493.9);
+
 function frequency(pitch) {
+gainNode.gain.setValueAtTime(0.8,audioCtx.currentTime);
+oscillator.frequency.setValueAtTime(pitch,audioCtx.currentTime);
+gainNode.gain.setValueAtTime(0,audioCtx.currentTime + 1);
 
 }
 function handle() {
-    frequency(input.value);
+    var usernotes = String(input.value);
+        var usernotes = String(input.value).toUpperCase();
+        var freq = notenames.get(usernotes);
+        if (freq) {
+            frequency(freq);
+        }
 }
