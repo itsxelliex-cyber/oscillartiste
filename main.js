fde75ba3fb44efceecdb67014dfc05c678731383
itsxelliex-cyber <itsxelliex@gmail.com>
2025-09-17 01:40:15 +0000

    reorganizing javascript so i can figure out why im getting bugs.. very helpful to add comments

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
+
+
+const input = document.getElementById('input');
 var amplitude = 40;
 var interval = null;
-const input = document.getElementById('input');
+var reset = false;
 const audioCtx = new AudioContext();
 const gainNode = audioCtx.createGain();
+
 const oscillator= audioCtx.createOscillator();
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+oscillator.type = "sine";
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var width = ctx.canvas.width;
 var height = ctx.canvas.height;
+
 oscillator.start();
 gainNode.gain.value = 0;
 audioCtx.resume();
 gainNode.gain.value = 0;
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-oscillator.type = "sine";
+
 notenames = new Map();
 notenames.set("C",261.6);
 notenames.set("D",293.7);
@@ -29,32 +36,47 @@ function frequency(pitch) {
     freq = pitch / 10000;
 gainNode.gain.setValueAtTime(0.8,audioCtx.currentTime);
 oscillator.frequency.setValueAtTime(pitch,audioCtx.currentTime);
-gainNode.gain.setValueAtTime(0,audioCtx.currentTime + 1);
+gainNode.gain.setValueAtTime(0,audioCtx.currentTime + 0.9);
 
 }
+
 function handle() {
-    var usernotes = String(input.value);
-        var usernotes = String(input.value).toUpperCase();
-        var freq = notenames.get(usernotes);
-        if (freq) {
-            frequency(freq);
-        }
+    reset = true;
+    var usernotes = String(input.value).toUpperCase();
+    var noteslist = [];
 
-        drawWave();
+        for (i = 0; i < usernotes.length; i++) {
+            noteslist.push(notenames.get(usernotes.charAt(i)));
+        }
+       let j = 0;
+       repeat = setInterval(() => {
+        if (j < noteslist.length) {
+            frequency(parseInt(noteslist[j]));
+            drawWave();
+        j++
+        } else {
+            clearInterval(repeat)
+        }
+        
+    }, 1000)
 }
+
 var counter = 0;
-function drawWave() {
+function drawWave(freq) {
+    clearInterval(interval);
+    if (reset) {
     ctx.clearRect(0, 0, width, height);
     x = 0;
     y = height / 2;
     ctx.moveTo(x, y);
     ctx.beginPath();
+    }
     counter = 0;
     interval = setInterval(line, 20);
-    if (counter > 50) {
-        clearInterval(interval);
+    reset = false;
     }
-}
+
+   
 function line() {
     y = height / 2 + (amplitude * Math.sin(x * 2 *
          Math.PI * freq));
@@ -64,4 +86,5 @@ function line() {
     //increase counter by 1 to show how long interval has been run
     counter++;
 }
+
 
