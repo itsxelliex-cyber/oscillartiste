85f8abb0bc53cfbf9c73febae1664a24509a2be7
itsxelliex-cyber <itsxelliex@gmail.com>
2025-09-19 01:51:39 +0000

    finally figured it out! im doneee might add edits later but this is good enough for submission

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
 
 const input = document.getElementById('input');
 
-const recording_toggle = 
-document.getElementById('record');
+const recording_toggle = document.getElementById('record');
 
 var timepernote = 0;
 var length = 0;
@@ -25,6 +24,38 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var width = ctx.canvas.width;
 var height = ctx.canvas.height;
+var counter = 0;
+function drawWave(freq) {
+    clearInterval(interval);
+    counter = 0;
+    if (reset) {
+    ctx.clearRect(0, 0, width, height);
+    x = 0;
+    y = height / 2;
+    ctx.moveTo(x, y);
+    ctx.beginPath();
+    }
+    
+    interval = setInterval(line, 20);
+    reset = false;
+    }
+
+   
+function line() {
+    y = height / 2 + vol_slider.value * Math.sin(x * ((2 *
+         Math.PI * freq * thickness_slider.value * (0.5 * length))));
+    ctx.strokeStyle = color_picker.value;
+    ctx.lineWidth = thickness_slider.value;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    x = x + 1;
+    //increase counter by 1 to show how long interval has been run
+    counter= counter + 1;
+
+    if(counter > (timepernote/20)) {
+        clearInterval(interval);
+    }
+}
 
 oscillator.start();
 gainNode.gain.value = 0;
@@ -51,7 +82,7 @@ const root = document.documentElement;
 function frequency(pitch) {
     freq = pitch / 10000;
 gainNode.gain.setValueAtTime((vol_slider.value/100)*40,audioCtx.currentTime);
-setting = setInterval(() => {gainNode.gain.value = (vol_slider.value/100)*40}, 1);
+setting = setInterval(() => {gainNode.gain.value =vol_slider.value}, + 1)
 oscillator.frequency.setValueAtTime(pitch,audioCtx.currentTime);
 setTimeout(() => { clearInterval(setting); gainNode.gain.value = 0; }, ((timepernote)-10));
 }
@@ -79,59 +110,23 @@ function handle() {
     }, timepernote);
 }
 
-var counter = 0;
-function drawWave(freq) {
-    clearInterval(interval);
-    if (reset) {
-    ctx.clearRect(0, 0, width, height);
-    x = 0;
-    y = height / 2;
-    ctx.moveTo(x, y);
-    ctx.beginPath();
-    }
-    counter = 0;
-    interval = setInterval(line, 20);
-    reset = false;
-    }
-
-   
-function line() {
-    y = height / 2 + vol_slider.value * Math.sin(x * ((2 *
-         Math.PI * freq * thickness_slider.value * (0.5 * length))));
-    ctx.strokeStyle = color_picker.value;
-    ctx.thicknessStyle = thickness_slider.value;
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    x = x + 1;
-    //increase counter by 1 to show how long interval has been run
-    counter++;
-
-    if(counter > (timepernote/20)) {
-        clearInterval(interval);
-    }
-}
 
 //js media recording api
 var blob, recorder = null;
-var chunks =[];
+var chunks = [];
 function startRecording() {
-   
     const canvasStream = canvas.captureStream(20);
     const audioDestination = audioCtx.createMediaStreamDestination(); 
     gainNode.connect(audioDestination);
-
     const combinedStream = new MediaStream();
-    chunks = [];
-
     canvasStream.getVideoTracks().forEach(track => combinedStream.addTrack(track));
-
     audioDestination.stream.getAudioTracks().forEach(track => combinedStream.addTrack(track));
-
     recorder = new MediaRecorder(combinedStream, { mimeType: 'video/webm' });
     recorder.ondataavailable = e => {
  if (e.data.size > 0) {
    chunks.push(e.data);
- };
+ }
+}
 
 recorder.onstop = () => {
    const blob = new Blob(chunks, { type: 'video/webm' });
@@ -141,25 +136,26 @@ recorder.onstop = () => {
    a.download = 'recording.webm';
    a.click();
    URL.revokeObjectURL(url);
-};
+}
+
 recorder.start();
 
-    }
+}
 
 var is_recording = false;
 function toggle() {
-    is_recording = !is_recording;
-    if(is_recording) {
-        recording_toggle.innerHTML = "Stop recording";
-        startRecording();
-    } else {
-        recording_toggle.innerHTML = "Start recording";
-        recorder.stop();
-    }
+   is_recording = !is_recording; 
+   if(is_recording){
+       recording_toggle.innerHTML = "Stop Recording";
+       startRecording(); 
+   } else {
+       recording_toggle.innerHTML = "Start Recording";
+       recorder.stop();
+   }
 }
 
 
 
-    }
+
 
 
